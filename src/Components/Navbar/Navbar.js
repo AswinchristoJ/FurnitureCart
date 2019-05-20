686a44b71b952b9777b4630bf20ea26a82b17dff
AswinChristo <aswinj2019@srishakthi.ac.in>
2019-05-20 15:35:46 +0530

    navbar component is created

---
+++ src/Components/Navbar/Navbar.js
@@ -0,0 +1,59 @@
+import React, { Component } from 'react';
+
+import {
+    Collapse,
+    Navbar,
+    NavbarToggler,
+    NavbarBrand,
+    Nav,
+    NavItem,
+    NavLink,
+} from 'reactstrap';
+
+class Navigationbar extends Component {
+
+    state = {
+        isOpen: false
+    };
+
+    toggle = () => {
+        this.setState({
+            isOpen: !this.state.isOpen
+        });
+    }
+
+    render() {
+
+        return (
+            <div>
+                <Navbar color="dark" dark expand="md">
+
+                    <NavbarBrand href="/">Furniture</NavbarBrand>
+                    <NavbarToggler onClick={this.toggle} />
+
+                    <Collapse isOpen={this.state.isOpen} navbar>
+
+                        <Nav className="ml-auto" navbar>
+
+                            <NavItem>
+                                <NavLink href="#">Query</NavLink>
+                            </NavItem>
+
+                            <NavItem>
+                                <NavLink href="#">Contact Us</NavLink>
+                            </NavItem>
+
+                            <NavItem>
+                                <NavLink href="#">AddToCart</NavLink>
+                            </NavItem>
+
+                            
+                        </Nav>
+                    </Collapse>
+                </Navbar>
+            </div>
+        );
+    }
+}
+
+export default Navigationbar;
