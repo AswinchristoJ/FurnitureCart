a53f4f5bfd64774ea472757b6aa71cbb2db1612d
AswinChristo <aswinj2019@srishakthi.ac.in>
2019-05-22 22:54:27 +0530

    local data set for testing

---
+++ src/data.js
@@ -0,0 +1,68 @@
+const data = [
+    {
+      "cost": 1,
+      "id": 1,
+      "link":"https://proxy.duckduckgo.com/iu/?u=http%3A%2F%2Fferpieandfray.files.wordpress.com%2F2014%2F10%2Fimage.jpg&f=1",
+      "title": "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
+      "description": "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto"
+    },
+    {
+      "cost": 1,
+      "id": 2,
+      "link":"https://proxy.duckduckgo.com/iu/?u=https%3A%2F%2Fcasefurniture.com%2Fwp-content%2Fuploads%2F2015%2F06%2FCovet_Desk_Shin_Azumi_OAK_lifestyle_2.jpg&f=1",
+      "title": "qui est esse",
+      "description": "est rerum tempore vitae\nsequi sint nihil reprehenderit dolor beatae ea dolores neque\nfugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis\nqui aperiam non debitis possimus qui neque nisi nulla"
+    },
+    {
+      "cost": 1,
+      "id": 3,
+      "link":"https://proxy.duckduckgo.com/iu/?u=http%3A%2F%2Fminecraftyoob.com%2Fwp-content%2Fuploads%2F2017%2F12%2Fklaussner-vaughn-sofa-74600sc-for-klaussner-furniture-reviews.jpg&f=1",
+      "title": "ea molestias quasi exercitationem repellat qui ipsa sit aut",
+      "description": "et iusto sed quo iure\nvoluptatem occaecati omnis eligendi aut ad\nvoluptatem doloribus vel accusantium quis pariatur\nmolestiae porro eius odio et labore et velit aut"
+    },
+    {
+      "cost": 1,
+      "id": 4,
+      "link":"https://proxy.duckduckgo.com/iu/?u=https%3A%2F%2Fi.pinimg.com%2F736x%2Fe7%2F71%2F42%2Fe7714246b5a9ae9037f405ad1102c784--maple-furniture-fine-furniture.jpg&f=1",
+      "title": "eum et est occaecati",
+      "description": "ullam et saepe reiciendis voluptatem adipisci\nsit amet autem assumenda provident rerum culpa\nquis hic commodi nesciunt rem tenetur doloremque ipsam iure\nquis sunt voluptatem rerum illo velit"
+    },
+    {
+      "cost": 1,
+      "id": 5,
+      "link":"https://proxy.duckduckgo.com/iu/?u=https%3A%2F%2Ffurnitureurban.com%2Fwp-content%2Fuploads%2F2016%2F06%2FASH81601-38-35-60-21-T820.jpg&f=1",
+      "title": "nesciunt quas odio",
+      "description": "repudiandae veniam quaerat sunt sed\nalias aut fugiat sit autem sed est\nvoluptatem omnis possimus esse voluptatibus quis\nest aut tenetur dolor neque"
+    },
+    {
+      "cost": 1,
+      "id": 6,
+      "link":"https://proxy.duckduckgo.com/iu/?u=http%3A%2F%2Fwww.gomodern.co.uk%2Fstore%2Ffiles%2Flarge-product-images%2FGM-ATLT-03-1-large.jpg&f=1",
+      "title": "dolorem eum magni eos aperiam quia",
+      "description": "ut aspernatur corporis harum nihil quis provident sequi\nmollitia nobis aliquid molestiae\nperspiciatis et ea nemo ab reprehenderit accusantium quas\nvoluptate dolores velit et doloremque molestiae"
+    },
+    {
+      "cost": 1,
+      "id": 7,
+      "link":"https://proxy.duckduckgo.com/iu/?u=http%3A%2F%2Frenaissancesociety.org%2Fmedia%2Ffiles%2F1972_prairie_181.jpg&f=1",
+      "title": "magnam facilis autem",
+      "description": "dolore placeat quibusdam ea quo vitae\nmagni quis enim qui quis quo nemo aut saepe\nquidem repellat excepturi ut quia\nsunt ut sequi eos ea sed quas"
+    },
+    {
+      "cost": 1,
+      "id": 8,
+      "link":"https://proxy.duckduckgo.com/iu/?u=https%3A%2F%2Fkozykingdom.com%2Fwp-content%2Fuploads%2F2016%2F02%2Fpub_set.jpg&f=1",
+      "title": "dolorem dolore est ipsam",
+      "description": "dignissimos aperiam dolorem qui eum\nfacilis quibusdam animi sint suscipit qui sint possimus cum\nquaerat magni maiores excepturi\nipsam ut commodi dolor voluptatum modi aut vitae"
+    },
+    {
+      "cost": 1,
+      "id": 9,
+      "link":"https://proxy.duckduckgo.com/iu/?u=http%3A%2F%2Fwww.mathisbrothers.com%2Fdw%2Fimage%2Fv2%2FAAYQ_PRD%2Fon%2Fdemandware.static%2F-%2FSites-mathisbrothers-master%2Fdefault%2Fdw5a5ca7bb%2Fimages%2Fproducts%2Fhires%2FASH%2FASH-4200038.jpg%3Fsw%3D1400%26sh%3D1000%26sm%3Dfit&f=1",
+      "title": "nesciunt iure omnis dolorem tempora et accusantium",
+      "description": "consectetur animi nesciunt iure dolore\nenim quia ad\nveniam autem ut quam aut nobis\net est aut quod aut provident voluptas autem voluptas"
+    },
+    
+  ]
+
+  export default data;
