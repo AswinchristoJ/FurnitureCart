0cfb592658fad576026ffcf8644d172ff55ab030
AswinChristo <aswinj2019@srishakthi.ac.in>
2019-06-01 20:36:45 +0530

    navigation bar is made fixed & layout background is changed

--- src/Components/Layout/Layout.js
+++ src/Components/Layout/Layout.js
@@ -6,8 +6,14 @@ const layout = (props) => {
 
     return (
         <div>
-            <Navigationbar />
-            <main>
+            <div style={{
+                position:"sticky",
+                top:"0",
+                zIndex:"1",
+            }}><Navigationbar /></div>
+            <main style={{
+                backgroundImage: "linear-gradient(to top, #accbee 0%, #e7f0fd 100%)"
+            }}>
                 {props.children}
             </main>
         </div>
